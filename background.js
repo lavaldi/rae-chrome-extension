9c9cb2bd31ee8afda251fb86f97a326736ace9ed
Claudia Valdivieso <claumavaldivieso@gmail.com>
2021-12-02 21:30:43 -0500

    format

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
-chrome.omnibox.onInputEntered.addListener(function(text) {
+chrome.omnibox.onInputEntered.addListener(function (text) {
   var newURL = "https://dle.rae.es/?w=" + encodeURIComponent(text);
   chrome.tabs.update({ url: newURL });
 });
